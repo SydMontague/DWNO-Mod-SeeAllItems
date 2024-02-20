f1c2e5bca17a3b51f4eb3df2701feb8966713906
SydMontague <sydmontague@phoenix-staffel.de>
2024-02-20 21:23:07 +0100

    initial commit

---
+++ Patches.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SeeAllItems
+{
+    class Patches
+    {
+        static bool activation_lock = false;
+
+        [HarmonyPatch(typeof(GameObject), "SetActive")]
+        [HarmonyPrefix]
+        static bool SetActive_Prefix(GameObject __instance)
+        {
+            return !activation_lock;
+        }
+
+        [HarmonyPatch(typeof(ItemPickPointManager), "UpdateNotUnity")]
+        [HarmonyPrefix]
+        static void UpdateNotUnity_Prefix(ItemPickPointManager __instance, float deltaTime, float time, float lastTime)
+        {
+            activation_lock = true;
+        }
+
+        [HarmonyPatch(typeof(ItemPickPointManager), "UpdateNotUnity")]
+        [HarmonyPostfix]
+        static void UpdateNotUnity_Postfix(ItemPickPointManager __instance, float deltaTime, float time, float lastTime)
+        {
+            activation_lock = false;
+            for (int k = 0; k < __instance.m_itemPickPoints.Count; k++)
+            {
+                ItemPickPointBase itemPickPointBase = __instance.m_itemPickPoints[k];
+                bool is_material = StorageData.m_itemPickPointData.GetMaterialPickPointData(__instance.m_itemPickPoints[k].id) != null;
+                float distance = is_material ? Plugin.Instance.materialRenderDistance.Value : Plugin.Instance.itemRenderDistance.Value;
+
+                if (Vector3.Distance(__instance.m_playerCtrl.transform.position, itemPickPointBase.transform.position) > distance)
+                {
+                    if (itemPickPointBase.gameObject.activeInHierarchy)
+                    {
+                        itemPickPointBase.gameObject.SetActive(false);
+                    }
+                }
+                else if (!itemPickPointBase.gameObject.activeInHierarchy)
+                {
+                    itemPickPointBase.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PartnerCtrl), "_UpdateItemPickPointsInRadar")]
+        [HarmonyPostfix]
+        static void UpdateItemRadar_Postfix(PartnerCtrl __instance)
+        {
+            Vector3 pos = __instance.transform.position;
+            List<PartnerCtrl.ItemPickPointsRadar> toRemove = new List<PartnerCtrl.ItemPickPointsRadar>();
+            foreach (var entry in __instance.m_ItemPickPointsRadar)
+            {
+                var itemPosition = entry.m_ItemPickPoint.gameObject.transform.position;
+                var distance = Vector3.Distance(itemPosition, pos);
+                if (distance > Plugin.Instance.itemRadarDistance.Value)
+                    toRemove.Add(entry);
+            }
+
+            foreach (var entry in toRemove)
+                __instance.m_ItemPickPointsRadar.Remove(entry);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System.ComponentModel;
+
+namespace SeeAllItems;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    public static Plugin Instance { get; private set; }
+
+    public ConfigEntry<float> itemRenderDistance { get; private set; }
+    public ConfigEntry<float> materialRenderDistance { get; private set; }
+    public ConfigEntry<float> itemRadarDistance { get; private set; }
+
+    public override void Load()
+    {
+        Instance = this;
+
+        itemRenderDistance = Config.Bind("General", "ItemRenderDistance", 200.0f, "Distance at which items become visible. Vanilla is 20.");
+        materialRenderDistance = Config.Bind("General", "MaterialRenderDistance", 200.0f, "Distance at which material spots become visible. Vanilla is 20.");
+        itemRadarDistance = Config.Bind("General", "ItemRadarDistance", 20.0f, "Distance at which your Digimon will notify you about items nearby. Vanilla is 20.");
+
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(typeof(Patches));
+    }
+}
